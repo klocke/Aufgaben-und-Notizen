97cee09d0fdc6741a06a5823b6f9673410cebf2b
Tobias <klocke.tobias@gmx.net>
2016-04-01 21:52:50 +0200

    Kommentare für Tim eingefügt

--- app/src/main/java/com/example/aufgabenundnotizen/other/DbActionTask.java
+++ app/src/main/java/com/example/aufgabenundnotizen/other/DbActionTask.java
@@ -54,12 +54,21 @@ public class DbActionTask extends AsyncTask<Void, Void, Integer> {
             if (mAction == Action.INSERT) {
                 mDatabaseAdapter.addItem(mItem);
 
+                // TODO @Tim
+                // in der Klasse, die sich um die Notifications kümmert, eine entspr. Methode registerNotification o.ä. aufrufen.
+
             } else if (mAction == Action.UPDATE) {
                 mDatabaseAdapter.updateItem(mItem);
+
+                // TODO @Tim
+                // in der Klasse, die sich um die Notifications kümmert, eine entspr. Methode updateNotification o.ä. aufrufen.
 
             } else if (mAction == Action.DELETE) {
                 Log.i("demo", "delete item on database");
                 mDatabaseAdapter.deleteItem(mItem);
+
+                // TODO @Tim
+                // in der Klasse, die sich um die Notifications kümmert, eine entspr. Methode removeNotification o.ä. aufrufen.
 
             }
         }
